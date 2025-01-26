b9d50979d0a17ebd4ac72ea9f97c9a1b9cc2ca70
DESKTOP-RF2SEO1\kosa <sinariari3@gmail.com>
2025-01-26 20:45:44 +0900

    feat: annotation을 통한 Member 객체 가져오기

--- bible/src/main/java/com/library/bible/config/WebMvcConfig.java
+++ bible/src/main/java/com/library/bible/config/WebMvcConfig.java
@@ -1,19 +1,30 @@
 package com.library.bible.config;
 
+import java.util.List;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.library.bible.commons.HttpMethodOverrideFilter;
+import com.library.bible.resolver.AuthMemberArgumentResolver;
 
 import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final AuthMemberArgumentResolver authMemberArgumentResolver;
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authMemberArgumentResolver);
+    }
 
     @Bean
     FilterRegistrationBean<HttpMethodOverrideFilter> httpMethodOverrideFilter() {

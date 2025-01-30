52a9cb8801a190be474bd11869d4d77b7860333d
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2025-01-30 17:29:58 +0900

    Feat: i18n 추가, dev 병합

--- bible/src/main/java/com/library/bible/config/WebMvcConfig.java
+++ bible/src/main/java/com/library/bible/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.library.bible.config;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import com.library.bible.commons.HttpMethodOverrideFilter;
 import com.library.bible.resolver.AuthMemberArgumentResolver;
@@ -49,5 +54,23 @@ public class WebMvcConfig implements WebMvcConfigurer {
             .allowedHeaders(corsConfiguration.getAllowedHeaders().toArray(String[]::new))
             .allowCredentials(corsConfiguration.getAllowCredentials());
     }
+    //i18n
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREAN); // 기본 언어 한국어 설정
+        return localeResolver;
+    }
 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang"); // URL에서 `?lang=en`으로 언어 변경 가능
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }

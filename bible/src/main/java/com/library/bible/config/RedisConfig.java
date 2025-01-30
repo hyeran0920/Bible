987cf266359ad96e016f60e8a2cdea384d1cc43f
soheekim273 <kimsohee980@naver.com>
2025-01-30 17:38:23 +0900

    feat: member 캐싱 기능 추가, redisConfig JSON 직렬화 기능 추가

--- bible/src/main/java/com/library/bible/config/RedisConfig.java
+++ bible/src/main/java/com/library/bible/config/RedisConfig.java
@@ -1,12 +1,17 @@
 package com.library.bible.config;
 
+import java.time.Duration;
+
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -29,5 +34,18 @@ public class RedisConfig {
         //Value 직렬화, JSON으로 저장
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
+    
+    // Spring cache에서도 JSON 직렬화 사용
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10)) // 캐시 만료 시간 (10분)
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
     }
 }

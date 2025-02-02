e41996a0a2147bce39a4428c4e3954268f026a86
kungu036 <sinariari3@gmail.com>
2025-02-02 15:03:00 +0900

    feat: member 유효성 추가 - back
    - Controller에서 memId 양수 검증 추가
    - model에서 email과 phone 검증 추가

--- bible/src/main/java/com/library/bible/member/model/Member.java
+++ bible/src/main/java/com/library/bible/member/model/Member.java
@@ -2,6 +2,8 @@ package com.library.bible.member.model;
 
 import java.util.List;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +15,12 @@ public class Member {
 	private int memId;
 	private String memName;
 	private String memPassword;
+	
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
 	private String memEmail;
+
+	@Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호 형식이 올바르지 않습니다. (010-XXXX-XXXX)")
+	private String memPhone;
+	
     private List<Role> roles;
 }

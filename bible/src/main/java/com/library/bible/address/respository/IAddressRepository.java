de8688eadea81838a735bc88336a5881d07d3ea3
kungu036 <sinariari3@gmail.com>
2025-02-04 11:12:20 +0900

    feat: address 테이블 변경사항 반영
    - id값의 type을 int에서 long으로 변경

--- bible/src/main/java/com/library/bible/address/respository/IAddressRepository.java
+++ bible/src/main/java/com/library/bible/address/respository/IAddressRepository.java
@@ -10,10 +10,10 @@ import com.library.bible.address.model.Address;
 @Mapper
 @Repository
 public interface IAddressRepository {
-    List<Address> selectAddressesByMemId(int memId);
-    Address seleAddress(int addressId);
+    List<Address> selectAddressesByMemId(long memId);
+    Address seleAddress(long addressId);
     int insertAddress(Address address);
     int updateAddress(Address address);
-    int deleteAddressesByMemId(int memId);
-    int deleteAddress(int addressId);
+    int deleteAddressesByMemId(long memId);
+    int deleteAddress(long addressId);
 }
--- bible/src/main/java/com/library/bible/address/service/IAddressService.java
+++ bible/src/main/java/com/library/bible/address/service/IAddressService.java
@@ -5,10 +5,10 @@ import java.util.List;
 import com.library.bible.address.model.Address;
 
 public interface IAddressService {
-    List<Address> selectAddressesByMemId(int memId);
-    Address seleAddress(int addressId);
+    List<Address> selectAddressesByMemId(long memId);
+    Address seleAddress(long addressId);
     Address insertAddress(Address address);
-    int updateAddress(Address address);
-    void deleteAddressesByMemId(int memId);
-    void deleteAddress(int addressId);    
+    Address updateAddress(Address address);
+    void deleteAddressesByMemId(long memId);
+    void deleteAddress(long addressId);    
 }

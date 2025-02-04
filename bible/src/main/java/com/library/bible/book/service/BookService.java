a43e49ffc64f26196d22d7fb9b5a77c865fe58f5
kungu036 <sinariari3@gmail.com>
2025-02-04 18:46:20 +0900

    feat: bookId로 Book 조회 추가 - back

--- bible/src/main/java/com/library/bible/book/service/BookService.java
+++ bible/src/main/java/com/library/bible/book/service/BookService.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -61,7 +60,9 @@ public class BookService implements IBookService {
     @Override
     @Cacheable(value = "books", key = "#bookId")
     public Book getBookInfo(Long bookId) {
-        return bookRepository.getBookInfo(bookId);
+        Book book = bookRepository.getBookInfo(bookId);
+        if(book == null) throw new CustomException(ExceptionCode.BOOK_NOT_FOUND);
+        return book;
     }
 
     // UPDATE, INSERT, DELETE

94c18b8c94a037fab0a5ef04402913d2774ed5ae
leesumin7766 <hojun7766@naver.com>
2025-02-06 14:27:05 +0900

    fix: 이전 commit 삭제 - back

--- bible/src/main/java/com/library/bible/rent/mapper/RentMapper.java
+++ bible/src/main/java/com/library/bible/rent/mapper/RentMapper.java
@@ -3,19 +3,11 @@ package com.library.bible.rent.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 import com.library.bible.rent.dto.RentResponse;
 import com.library.bible.rent.model.Rent;
 
 @Mapper(componentModel = "spring")
 public interface RentMapper {
-    RentMapper INSTANCE = Mappers.getMapper(RentMapper.class);
-
-    RentResponse toRentResponse(Rent rent);
     List<RentResponse> toRentResponseList(List<Rent> rentList);
-    default List<RentResponse> rentsToRentResponses(List<Rent> rentList) {
-        return toRentResponseList(rentList);
-    }
 }

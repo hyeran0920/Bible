19da1fce63f8535eeb86ded187aeb33b0500e665
Jin <hyeran0920@naver.com>
2025-02-07 21:13:27 +0900

    feat : BestSeller 스케줄링

--- bible/src/main/java/com/library/bible/alarm/service/NotificationService.java
+++ bible/src/main/java/com/library/bible/alarm/service/NotificationService.java
@@ -1,12 +1,17 @@
 package com.library.bible.alarm.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.library.bible.book.service.BookService;
+import com.library.bible.book.service.IBookService;
 import com.library.bible.rent.dto.RentMemberResponse;
+import com.library.bible.rent.service.IRentService;
 import com.library.bible.rent.service.RentService;
 
 @Service
@@ -15,23 +20,28 @@ public class NotificationService {
     // ‘*’는 모든값
 	// ‘?’는	사용 안함,	‘-’는 기간, ‘,’는 값 지정+나열, ‘/’는 시작/반복 간격
 	private final SmsService smsService;
-    private final RentService rentService;
+    private final IRentService rentService;
+    private final IBookService bookService;
 
-    public NotificationService(SmsService smsService, RentService rentService) {
+    public NotificationService(SmsService smsService, IRentService rentService, IBookService bookService ) {
         this.smsService = smsService;
 		this.rentService = rentService;
+		this.bookService = bookService;
     }
 
+    //전송하는거
     private void sendSmsNotification(String phone, String text) {
-        SmsService.sendSms(phone, text); //이거 활성화하면 정해진 시간에 문자가 갑니다
+//        SmsService.sendSms(phone, text); //이거 활성화하면 정해진 시간에 문자가 갑니다
     }
     
-    public void sendOverdueNotifications() {
+    //연체된게 있는지 확인하기
+    public void checkOverdueNotifications() {
         List<String> overdueMessages = rentService.processOverdueBooks();
         for (String message : overdueMessages) {
         }
     }
 
+    //연체된게 있으면 문자 보내기
     public void sendOverdueCheck() {
         List<String> overdueMessages = rentService.processOverdueBooks();    
         for (String message : overdueMessages) {
@@ -60,6 +70,17 @@ public class NotificationService {
         }
     }
 
+    //베스트셀러 업데이트
+    public void checkBestSeller() {
+        //베스트셀러 북 리스트 가져오기
+        long[] bookIdArr = bookService.getBestSellerBookIdArray();	// 순위별 bookId Array => [ 1, 2, 3, 4, ..]
+        List<Map<String, Object>> bestSellerList = new ArrayList<>();
+        
+        for(long bookId : bookIdArr) {
+            bestSellerList.add(bookService.getBookInfoMap(bookId));
+        }
+        
+    }
 
 }
    
--- bible/src/main/java/com/library/bible/alarm/service/ScheduleService.java
+++ bible/src/main/java/com/library/bible/alarm/service/ScheduleService.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.library.bible.book.repository.IBookRepository;
+import com.library.bible.book.service.BookService;
 import com.library.bible.rent.repository.IRentRepository;
 import com.library.bible.rent.service.IRentService;
 import com.library.bible.rent.service.RentService;
@@ -16,19 +18,19 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class ScheduleService {
 
-    private final RentService rentService;
     private final NotificationService notificationService;
     
     
-    public ScheduleService(RentService rentService, NotificationService notificationService) {
-        this.rentService = rentService;
+    public ScheduleService(NotificationService notificationService) {
         this.notificationService = notificationService;
     }
 
-    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행
+    @Scheduled(cron = "0 12 21 * * ?") // 매일 자정에 실행
     public void scheduleOverdueCheck() {
-    	System.out.println("연체 확인");
-    	notificationService.sendOverdueNotifications();
+    	System.out.println("자정 스케줄링 실행");
+    	notificationService.checkOverdueNotifications();
+    	notificationService.checkBestSeller();
+    	
     }
     
     @Scheduled(cron = "0 0 11 * * ?") // 매일 자정에 실행

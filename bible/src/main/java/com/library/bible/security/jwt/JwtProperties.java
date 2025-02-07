bb50fd9a8c8c1a21dc541548ee715a802b5725c1
kungu036 <sinariari3@gmail.com>
2025-02-07 14:27:10 +0900

    feat: JWT key 환경변수로 변경 - back

--- bible/src/main/java/com/library/bible/security/jwt/JwtProperties.java
+++ bible/src/main/java/com/library/bible/security/jwt/JwtProperties.java
@@ -1,7 +1,10 @@
 package com.library.bible.security.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface JwtProperties {
 	// access token
+    @Value("${spring.jwt.access}")
 	String ACCESS_SECRET = "cos1"; // 우리 서버만 알고 있는 비밀값
 	//int ACCESS_EXPIRATION_TIME = 10 * 60 * 1000; // 10분 (1/1000초)
 	int ACCESS_EXPIRATION_TIME = 1000 * 60 * 1000; 
@@ -9,6 +12,7 @@ public interface JwtProperties {
 	String ACCESS_COOKIE_STRING = "authorization";
 	
 	// refresh token
+    @Value("${spring.jwt.refresh}")
 	String REFRESH_SECRET = "cos2"; // 우리 서버만 알고 있는 비밀값
 	int REFRESH_EXPIRATION_TIME = 7 * 24 * 60 * 60 * 1000; // 7일
 	String REFRESH_HEADER_STRING = "Refresh-Token";

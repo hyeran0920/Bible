4d4c148b51966c99977fd5cb9af9846f1fbbfb13
leesumin7766 <hojun7766@naver.com>
2025-02-11 12:02:25 +0900

    fix: FlaskClientService.java

--- bible/src/test/java/com/library/bible/FlaskApi/FlaskClientServiceTest.java
+++ bible/src/test/java/com/library/bible/FlaskApi/FlaskClientServiceTest.java
@@ -16,8 +16,9 @@ class FlaskClientServiceTest {
     @Test
     void testFlaskApiCall() {
         // Flask API에서 응답을 받아오는 메서드를 호출 //
-        String response = flaskClientService.getRecommendation(1, 5);
+        String response = flaskClientService.getRecommendation(1030, 10);
         
+        System.out.println("Flask API 응답: " + response);
         assertNotNull(response, "not Null.");
         assertTrue(response.startsWith("http"), "유효한 URL이 아닙니다.");   
     }

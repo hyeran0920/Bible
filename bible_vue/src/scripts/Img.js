0ee739be6ccde5b03ea4570961990f1ca08b1bea
yunji <yunji378mm@naver.com>
2025-02-21 10:02:32 +0900

    fix:imgs
    book img
    book qr img
    member qr img
    로컬에 저장하고 관리하게 변경

--- bible_vue/src/scripts/Img.js
+++ bible_vue/src/scripts/Img.js
@@ -1,17 +1,24 @@
-
+import axiosInstance from "../plugins/axios"; 
 
 
 const ImageUtils = {
     getBookImg(bookId) {
-        //return `https://bibleblob.blob.core.windows.net/images/${bookId}.jpg`;
-        return `/images/bookimg/${bookId}.jpg`;  // public 폴더 기준
-        return `https://ifh.cc/g/Oc2y14.jpg`;
+        //return `https://bibleblob.blob.core.windows.net/images/${bookId}.jpg`; //azure!!!!!
+
+        console.log("Axios Base URL:", axiosInstance.defaults.baseURL);
+        return `${axiosInstance.defaults.baseURL}/uploads/book-image?bookid=${bookId}`;
+
+        //return `/images/bookimg/${bookId}.jpg`;  // public 폴더 기준
+        //return `https://ifh.cc/g/Oc2y14.jpg`; //더미 이미지
         
     },
     getBookQRImg(bookId) {
         //return `https://bibleblob.blob.core.windows.net/bookqr/${bookId}.png`;
-        return `/images/bookqr/${bookId}.jpg`;  // public 폴더 기준
-        return `https://ifh.cc/g/WdcvqT.png`;
+        
+        
+        return `${axiosInstance.defaults.baseURL}/uploads/book-qr-image?bookid=${bookId}`;
+        //return `/images/bookqr/${bookId}.jpg`;  // public 폴더 기준
+        //return `https://ifh.cc/g/WdcvqT.png`;
     },
     getMemberQRImg(memId) {
         if(memId==null){
@@ -19,6 +26,8 @@ const ImageUtils = {
         }
         //return `https://bibleblob.blob.core.windows.net/memberqr/${memId}.png`;
 
+        console.log("get member qr img=",memId);
+        return `${axiosInstance.defaults.baseURL}/uploads/member-qr-image/mem?memid=${memId}`;
         //return `/images/memberqr/${memId}.jpg`;  // public 폴더 기준
         return `https://ifh.cc/g/WdcvqT.png`;
     }
